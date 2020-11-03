30161bad9a67bedd39a10d52d83f7bc2f9707ec6
Elizabeth Sajdak <esajdak@madisoncollege.edu>
2020-11-02 20:07:46 -0600

    Everything in user working with lombok implemented

--- src/main/java/edu/matc/entity/User.java
+++ src/main/java/edu/matc/entity/User.java
@@ -1,5 +1,6 @@
 package edu.matc.entity;
 
+import lombok.*;
 import org.hibernate.annotations.GenericGenerator;
 
 import javax.persistence.*;
@@ -12,6 +13,8 @@ import java.util.Set;
  */
 @Entity(name = "User")
 @Table(name = "user")
+@Getter @Setter @NoArgsConstructor
+@ToString @EqualsAndHashCode
 public class User {
     @Column(name = "first_name")
     private String firstName;
@@ -32,29 +35,10 @@ public class User {
     private int userId;
 
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.EAGER)
-    private Set<Product> products = new HashSet<>();
+    @ToString.Exclude @EqualsAndHashCode.Exclude private Set<Product> products = new HashSet<>();
 
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.EAGER)
-    private Set<Role> roles = new HashSet<>();
-
-    /**
-     * Instantiates a new User.
-     */
-    public User() {
-
-    }
-
-//    @Override
-//    public String toString() {
-//        return "User{" +
-//                "firstName='" + firstName + '\'' +
-//                ", lastName='" + lastName + '\'' +
-//                ", email='" + email + '\'' +
-//                ", password='" + password + '\'' +
-//                ", userId=" + userId +
-//                ", products=" + products +
-//                '}';
-//    }
+    @ToString.Exclude @EqualsAndHashCode.Exclude private Set<Role> roles = new HashSet<>();
 
     /**
      * Instantiates a new User.
@@ -69,114 +53,6 @@ public class User {
         this.lastName = lastName;
         this.email = email;
         this.password = password;
-    }
-
-    /**
-     * Sets first name
-     *
-     * @param firstName user's first name
-     */
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    /**
-     * Gets first name
-     *
-     * @return user 's first name
-     */
-    public String getFirstName() {
-        return firstName;
-    }
-
-    /**
-     * Sets last name
-     *
-     * @param lastName user's last name
-     */
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    /**
-     * Gets last name
-     *
-     * @return user 's last name
-     */
-    public String getLastName() {
-        return lastName;
-    }
-
-    /**
-     * Sets email
-     *
-     * @param email user's email
-     */
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    /**
-     * Gets email
-     *
-     * @return user 's email
-     */
-    public String getEmail() {
-        return email;
-    }
-
-    /**
-     * Sets password
-     *
-     * @param password user's password
-     */
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    /**
-     * Gets password
-     *
-     * @return user 's password
-     */
-    public String getPassword() {
-        return password;
-    }
-
-    /**
-     * Sets user id
-     *
-     * @param userId user's ID
-     */
-    public void setUserId(int userId) {
-        this.userId = userId;
-    }
-
-    /**
-     * Gets user id
-     *
-     * @return user 's id
-     */
-    public int getUserId() {
-        return userId;
-    }
-
-    /**
-     * Gets products.
-     *
-     * @return the products
-     */
-    public Set<Product> getProducts() {
-        return products;
-    }
-
-    /**
-     * Sets products.
-     *
-     * @param products the products
-     */
-    public void setProducts(Set<Product> products) {
-        this.products = products;
     }
 
     /**
@@ -199,39 +75,23 @@ public class User {
         product.setUser(null);
     }
 
-
-    public Set<Role> getRoles() {
-        return roles;
-    }
-
-    public void setRoles(Set<Role> roles) {
-        this.roles = roles;
-    }
-
+    /**
+     * Add role.
+     *
+     * @param role the role
+     */
     public void addRole(Role role) {
         roles.add(role);
         role.setUser(this);
     }
 
+    /**
+     * Remove role.
+     *
+     * @param role the role
+     */
     public void removeRole(Role role) {
         roles.remove(role);
         role.setUser(null);
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        User user = (User) o;
-        return userId == user.userId &&
-                Objects.equals(firstName, user.firstName) &&
-                Objects.equals(lastName, user.lastName) &&
-                Objects.equals(email, user.email) &&
-                Objects.equals(password, user.password);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(firstName, lastName, email, password, userId);
     }
 }
